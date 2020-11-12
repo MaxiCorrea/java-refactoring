a58324be66eab11748027f0654e138a1b285f75a
Maximiliano Correa <jmaxicorrea@gmail.com>
2020-11-11 23:17:37 -0300

    Replace Temp with query method (Total Frequent Renter Points)

--- src/main/java/chapter001/refactorcode/Customer.java
+++ src/main/java/chapter001/refactorcode/Customer.java
@@ -30,16 +30,23 @@ public class Customer {
 
   public String statement() {
     double totalAmount = 0;
-    int frequentRenterPoints = 0;
     String result = "Rental Record for " + getName() + "\n";
     for (Rental each : rentals) {
-      frequentRenterPoints += each.getFrequentRenterPoints();
       result += "\t" + each.getMovie().getTitle() + "\t" + each.getCharge() + "\n";
       totalAmount += each.getCharge();
     }
     result += "Amount owed is " + totalAmount + "\n";
-    result += "You earned " + frequentRenterPoints + " frequent renter points";
+    result += "You earned " + getTotalFrequentRenterPoints() + " frequent renter points";
     return result;
   }
 
+  private int getTotalFrequentRenterPoints() {
+    int total = 0;
+    for (Rental each : rentals) {
+      total += each.getFrequentRenterPoints();
+    }
+    return total;
+  }
+  
+  
 }
