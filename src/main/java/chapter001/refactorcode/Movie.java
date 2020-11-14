045857e15f8b75913111d9779042d4948a5cd887
Maximiliano Correa <jmaxicorrea@gmail.com>
2020-11-13 21:53:47 -0300

    Move Method to Abstract Price Class

--- src/main/java/chapter001/refactorcode/Movie.java
+++ src/main/java/chapter001/refactorcode/Movie.java
@@ -53,21 +53,7 @@ public class Movie {
 
   public double getCharge(
       final int daysRented) {
-    double result = 0;
-    if (getPriceCode() == Movie.NEW_RELEASE) {
-      result += daysRented * 3;
-    } else if (getPriceCode() == Movie.CHILDRENS) {
-      result += 1.5;
-      if (daysRented > 3) {
-        result += (daysRented - 3) * 1.5;
-      }
-    } else if (getPriceCode() == Movie.REGULAR) {
-      result += 2;
-      if (daysRented > 2) {
-        result += (daysRented - 2) * 1.5;
-      } 
-    }
-    return result;
+    return price.getCharge(daysRented);
   }
 
   public int getFrequentRenterPoints(
--- src/main/java/chapter001/refactorcode/Price.java
+++ src/main/java/chapter001/refactorcode/Price.java
@@ -3,5 +3,24 @@ package chapter001.refactorcode;
 public abstract class Price {
 
   public abstract int getPriceCode();
+
+  public double getCharge(
+      final int daysRented) {
+    double result = 0;
+    if (getPriceCode() == Movie.NEW_RELEASE) {
+      result += daysRented * 3;
+    } else if (getPriceCode() == Movie.CHILDRENS) {
+      result += 1.5;
+      if (daysRented > 3) {
+        result += (daysRented - 3) * 1.5;
+      }
+    } else if (getPriceCode() == Movie.REGULAR) {
+      result += 2;
+      if (daysRented > 2) {
+        result += (daysRented - 2) * 1.5;
+      } 
+    }
+    return result;
+  }
   
 }
