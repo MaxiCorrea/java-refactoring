8f7f7e5ddf4dcb54bda09efa2e6e997c31021067
Maximiliano Correa <jmaxicorrea@gmail.com>
2020-11-14 04:28:09 -0300

    outstanding atribute to local variable

--- src/main/java/composingmethods/extractmethod/InvoicePrinter.java
+++ src/main/java/composingmethods/extractmethod/InvoicePrinter.java
@@ -6,31 +6,26 @@ public class InvoicePrinter {
 
   private String name;
   private List<Item> items;
-  private double outstanding;
   
   public InvoicePrinter(
       final String name,
       final List<Item> items) {
     this.name = name;
     this.items = items;
-    this.outstanding = 0.0;
   }
   
   public String getName() { 
     return name;
   }
   
-  public double getOutstanding() {
-    return outstanding;
-  }
-  
   public String print() {
     String output = printBanner();
     
+    double outstanding = 0.0;
     for(Item item : items) {
       outstanding += item.getAmount();
     }
-    
+  
     output += "name : " + name + "\n";
     output += "amount : " + outstanding + "\n";
     return output;
