40d0475461e8f90e1b33c7027dba578be4ab9028
Maximiliano Correa <jmaxicorrea@gmail.com>
2020-11-21 23:12:45 -0300

    Adding package-info.java

---
+++ src/main/java/composingmethods/extractmethod/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * Tecnica de Refactoring "Extract Method"
+ * 
+ * Motivacion para realizar este refactoring.
+ * 
+ * a) Un metodo muy largo que necesita algun tipo de comentaria
+ *    en el cuerpo del mismo.
+ * 
+ * b) Necesidad de reutilizar parte de la funcionacional de un 
+ *    metodo.    
+ * 
+ * c) Para mejorar la claridad de las acciones que realizar el
+ *    metodo.
+ *    
+ */
+package composingmethods.extractmethod;
