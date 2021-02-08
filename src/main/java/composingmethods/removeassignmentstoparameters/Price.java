533103e4e48faf34029556bfcd92c23ea3ed303d
Maximiliano Correa <jmaxicorrea@gmail.com>
2021-02-07 23:58:59 -0300

    Appy Refactor

--- src/main/java/composingmethods/removeassignmentstoparameters/Price.java
+++ src/main/java/composingmethods/removeassignmentstoparameters/Price.java
@@ -2,14 +2,18 @@ package composingmethods.removeassignmentstoparameters;
 
 public final class Price {
 
-  public final int discount(int inputVal, int quantity, int yearToDate) {
+  public final int discount(
+      final int inputVal, 
+      final int quantity, 
+      final int yearToDate) {
+    int result = inputVal;
     if (inputVal > 50)
-      inputVal -= 2;
+      result -= 2;
     if (quantity > 100)
-      inputVal -= 1;
+      result -= 1;
     if (yearToDate > 10000)
-      inputVal -= 4;
-    return inputVal;
+      result -= 4;
+    return result;
   }
 
 }
