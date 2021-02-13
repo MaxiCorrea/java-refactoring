a9cc3ba60b9205c821be95ae4f138e562e88d83d
Maximiliano Correa <jmaxicorrea@gmail.com>
2021-02-13 19:53:51 -0300

    Sustitute Algorithm Refactoring Info

---
+++ src/main/java/composingmethods/substitutealgorithm/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * Si encuentra una forma más clara de hacer algo, debe reemplazar la forma complicada con la forma
+ * más clara. La refactorización puede dividir algo complejo en partes más simples, pero a veces
+ * simplemente llega al punto en el que tiene que eliminar todo el algoritmo y reemplazarlo con algo
+ * más simple. Esto ocurre a medida que aprende más sobre el problema y se da cuenta de que hay una
+ * manera más fácil de solucionarlo. También sucede si comienza a usar una biblioteca que
+ * proporciona funciones que duplican su código.
+ * 
+ * A veces, cuando desea cambiar el algoritmo para hacer algo ligeramente diferente, es más fácil
+ * sustituir el algoritmo primero en algo más fácil para el cambio que necesita hacer. Cuando tenga
+ * que dar este paso, asegúrese de haber descompuesto el método tanto como pueda. Sustituir un
+ * algoritmo grande y complejo es muy difícil; solo haciéndolo simple puede hacer que la sustitución
+ * sea manejable.
+ */
+package composingmethods.substitutealgorithm;
