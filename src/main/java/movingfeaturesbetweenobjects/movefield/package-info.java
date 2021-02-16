9e7b31649eb3d94489d00b0deabdefaf362d04f3
Maximiliano Correa <jmaxicorrea@gmail.com>
2021-02-16 01:49:18 -0300

    Create Package for Move Field Refactoring Example

---
+++ src/main/java/movingfeaturesbetweenobjects/movefield/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Move Field
+ * 
+ * Un campo es, o será, usado por otra clase más que la clase en la que está definido.
+ * 
+ * Cree un nuevo campo en la clase de destino y cambie todos sus usuarios.
+ * 
+ */
+package movingfeaturesbetweenobjects.movefield;
