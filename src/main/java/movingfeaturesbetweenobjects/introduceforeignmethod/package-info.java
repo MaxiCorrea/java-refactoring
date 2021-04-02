9765dce6873eb0aa25d626dd44eb4bb343d28cca
Maximiliano Correa <jmaxicorrea@gmail.com>
2021-04-02 14:32:39 -0300

    Introduce Foreing Method [intro]

---
+++ src/main/java/movingfeaturesbetweenobjects/introduceforeignmethod/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Una clase de servidor que está utilizando necesita un método adicional, pero no puede modificar
+ * la clase. Cree un método en la clase de cliente con una instancia de la clase de servidor como
+ * primer argumento.
+ */
+package movingfeaturesbetweenobjects.introduceforeignmethod;
