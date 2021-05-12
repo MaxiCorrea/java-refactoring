e7412c7d27ed03972627b80aaa9996aa27f0c1c3
Maximiliano Correa <jmaxicorrea@gmail.com>
2021-05-12 20:08:09 -0300

    Replace Array With Object

---
+++ src/main/java/organizingdata/replacearraywithobject/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * Tienes una matriz en la que ciertos elementos significan cosas diferentes. Reemplace la matriz
+ * con un objeto que tenga un campo para cada elemento.
+ * 
+ * 
+ * Las matrices son una estructura común para organizar datos. Sin embargo, deben usarse solo para
+ * contener una colección de objetos similares en algún orden. A veces, sin embargo, ves que solían
+ * contener una serie de cosas diferentes. Convenciones como "el primer elemento de la matriz es la
+ * persona name "son difíciles de recordar. Con un objeto, puede usar nombres de campos y métodos
+ * para transmitir esta información para que no tenga que recordarla o esperar que los comentarios
+ * estén actualizados. Usted puede también encapsula la información y usa el método Move para
+ * agregarle comportamiento.
+ */
+package organizingdata.replacearraywithobject;
