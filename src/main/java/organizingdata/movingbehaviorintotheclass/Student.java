33eca8842d66c5c0ebc8dda30b1e3492be29deed
Maximiliano Correa <jmaxicorrea@gmail.com>
2021-05-23 00:24:32 -0300

    Moving Behavior into the Class (Initial Code for exampĺe)

---
+++ src/main/java/organizingdata/movingbehaviorintotheclass/Student.java
@@ -0,0 +1,47 @@
+package organizingdata.movingbehaviorintotheclass;
+
+import static java.util.Collections.unmodifiableSet;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 
+ * @author mxcorrea
+ *
+ */
+public final class Student {
+
+  private String name;
+  private Set<Course> courses;
+  
+  public Student(
+      final String name) {
+    this.name = name;
+    this.courses = new HashSet<>();
+  }
+  
+  public void initializeCourses(
+      final Set<Course> courses) {
+    assert this.courses.isEmpty();
+    this.courses.addAll(courses);
+  }
+  
+  public String getName() {
+    return name;
+  }
+  
+  public void addCourse(
+      final Course course) {
+    this.courses.add(course);
+  }
+  
+  public void removeCourse(
+      final Course course) {
+    this.courses.remove(course);
+  }
+  
+  public Set<Course> getCourses() {
+    return unmodifiableSet(this.courses);
+  }
+   
+}
---
+++ src/test/java/organizingdata/movingbehaviorintotheclass/StudentTest.java
@@ -0,0 +1,47 @@
+package organizingdata.movingbehaviorintotheclass;
+
+import static org.junit.Assert.*;
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.Test;
+/**
+ * t;
+ * @author mxcorrea
+ *
+ */
+public class StudentTest {
+
+  @Test
+  public void testingAddAndRemoveCourses() {
+    Student kent = new Student("Kent");
+    Set<Course> s = new HashSet<>();
+    s.add(new Course ("Smalltalk Programming", false));
+    s.add(new Course ("Appreciating Single Malts", true));
+    kent.initializeCourses(s);
+    assertEquals(2, kent.getCourses().size());
+    Course refact = new Course ("Refactoring", true);
+    kent.addCourse(refact);
+    kent.addCourse(new Course ("Brutal Sarcasm",false));
+    assertEquals (4, kent.getCourses().size());
+    kent.removeCourse(refact);
+    assertEquals (3, kent.getCourses().size());
+  }
+
+  @Test
+  public void testingFoundAdvancedCources() {
+    Student kent = new Student("Kent");
+    Set<Course> s = new HashSet<>();
+    s.add(new Course ("Smalltalk Programming", false));
+    s.add(new Course ("Appreciating Single Malts", true));
+    s.add(new Course ("Functional Programming", true));
+    kent.initializeCourses(s);
+    int expectedAdvancedCourses = 2;
+    int actualAdvancedCourses = 0;
+      for(Course c : kent.getCourses()) 
+        if(c.isAdvance()) 
+          ++actualAdvancedCourses;
+    assertEquals(expectedAdvancedCourses , actualAdvancedCourses);
+  }
+  
+
+}
