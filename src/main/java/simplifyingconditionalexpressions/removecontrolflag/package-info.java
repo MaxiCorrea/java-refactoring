ac2ebfedde149cdd2419f2393d6b5265d08e0efc
Maximiliano Correa <jmaxicorrea@gmail.com>
2021-05-28 19:15:07 -0300

    Remove Control Flag (Motivation)

---
+++ src/main/java/simplifyingconditionalexpressions/removecontrolflag/package-info.java
@@ -0,0 +1,17 @@
+/**
+ * Tiene una variable que actúa como indicador de control (Flag) para una serie de expresiones
+ * booleanas.
+ * 
+ * En su lugar, utilice un "break" o un "return"
+ * 
+ * 
+ * Tales banderas de control son más problemáticas de lo que valen. Vienen de reglas de programación
+ * estructurada que requieren rutinas con una entrada y un punto de salida. Estoy de acuerdo con (y
+ * los lenguajes modernos imponen) un punto de entrada, pero la regla de un punto de salida te lleva
+ * a condicionales muy complicados con estos indicadores incómodos en el código. Es por eso que los
+ * lenguajes tienen declaraciones break y continue para salir de un condicional complejo. A menudo
+ * es sorprendente lo que puede hacer cuando se deshace de una bandera de control. El verdadero
+ * propósito del condicional se vuelve mucho más claro.
+ * 
+ */
+package simplifyingconditionalexpressions.removecontrolflag;
