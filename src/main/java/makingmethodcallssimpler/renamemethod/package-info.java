813853d3da2bc2411af5b3c6615089e7e853e153
Maximiliano Correa <jmaxicorrea@gmail.com>
2021-05-31 12:41:40 -0300

    Rename Method (Motivation)

---
+++ src/main/java/makingmethodcallssimpler/renamemethod/package-info.java
@@ -0,0 +1,18 @@
+/**
+ * Una parte importante del estilo de código que defiendo son los métodos pequeños para factorizar
+ * procesos complejos. Si se hace mal, esto puede llevarlo a un baile alegre para descubrir qué
+ * hacen todos los pequeños métodos. La clave para evitar este alegre baile es nombrar los métodos.
+ * Los métodos deben nombrarse de manera que comuniquen su intención. Una buena forma de hacerlo es
+ * pensar cuál sería el comentario del método y convertir ese comentario en el nombre del método.
+ * 
+ * Siendo la vida lo que es, no entenderás bien tus nombres la primera vez. En esta situación, es
+ * muy posible que sienta la tentación de dejarlo; después de todo, es sólo un nombre. Esa es la
+ * obra del demonio malvado Obfuscatis; no le escuches. Si ve un método con un nombre incorrecto, es
+ * imperativo que lo cambie. Recuerde que su código es para un humano primero y luego para una
+ * computadora. Los humanos necesitan buenos nombres. Tome nota de cuándo ha pasado años tratando de
+ * hacer algo que hubiera sido más fácil si se hubiera nombrado mejor un par de métodos. La buena
+ * denominación es una habilidad que requiere práctica; mejorar esta habilidad es la clave para ser
+ * un programador verdaderamente hábil. Lo mismo se aplica a otros aspectos de la firma. Si
+ * reordenar los parámetros aclara las cosas, hágalo
+ */
+package makingmethodcallssimpler.renamemethod;
