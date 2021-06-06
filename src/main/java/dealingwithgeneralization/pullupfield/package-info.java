b01ec9a0e7392bbd3342baf3aaf1c3b2ec8e35c8
Maximiliano Correa <jmaxicorrea@gmail.com>
2021-06-06 04:33:40 -0300

    Adding Motivation for Refactoring to package-info.java

--- src/main/java/dealingwithgeneralization/pullupfield/package-info.java
+++ src/main/java/dealingwithgeneralization/pullupfield/package-info.java
@@ -2,5 +2,11 @@
  * Dos subclases tienen el mismo campo/atributo
  * 
  * Suba el campo a la superclase!
+ * 
+ * Si dos subclases fueron desarrolladas de forma independiente o luego combinadas, puedes encontrar
+ * caracteristicas duplicadas,en particular algunos campos pueden estar duplicados. Aveces los
+ * campos tienen los mismos nombres pero no son lo mismos. Debes observar que codigo lo utiliza y si
+ * se utiliza para los mismo que algun otro campos de una clase "hermana" podemos generalizar
+ * subiendo el codigo a la clase padre.
  */
 package dealingwithgeneralization.pullupfield;
