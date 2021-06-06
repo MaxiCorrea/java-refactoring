e1683ed89a47c3638e43828b4b693f94f4c72d7b
MaxiCorrea <jmaxicorrea@gmail.com>
2021-06-06 17:10:04 -0300

    Adding Unit Test Before Refactoring!

---
+++ src/test/java/dealingwithgeneralization/extractsubclass/JobItemTest.java
@@ -0,0 +1,30 @@
+package dealingwithgeneralization.extractsubclass;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+/**
+ * 
+ * @author mxcorrea
+ *
+ */
+public class JobItemTest {
+
+  private static final Employee employee = new Employee(10);
+  
+  @Test
+  public void theUnitPriceForAJobWithoutAnEmployee() {
+    JobItem item = new JobItem(300, 2, false, employee);
+    int expectedUnitPrice = 300;
+    int actualUnitPrice = item.getUnitPrice();
+    assertEquals(expectedUnitPrice , actualUnitPrice);
+  }
+
+  @Test
+  public void theUnitPriceForAJobWithAnEmployee() {
+    JobItem item = new JobItem(300, 2, true, employee);
+    int expectedUnitPrice = 10;
+    int actualUnitPrice = item.getUnitPrice();
+    assertEquals(expectedUnitPrice , actualUnitPrice);
+  }
+  
+}
