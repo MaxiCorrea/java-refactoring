79ab0af3531aa22298a7d493ad88c17a3981a228
Maximiliano Correa <jmaxicorrea@gmail.com>
2021-06-06 04:00:52 -0300

    Update package-info.java

--- src/main/java/makingmethodcallssimpler/replaceexceptionwithtest/package-info.java
+++ src/main/java/makingmethodcallssimpler/replaceexceptionwithtest/package-info.java
@@ -1,4 +1,7 @@
 /**
+ * Reemplace Excepton con Test:
  * 
+ * Las exception deben ser utilizadas para casos donde no tenemos el control desde nuestro codigo.
+ * Si podemos haces testeos o validaciones deberiamos evitar las exceptions.
  */
 package makingmethodcallssimpler.replaceexceptionwithtest;
