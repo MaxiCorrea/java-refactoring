287d1a46ed4c4023990863e6beb21d11b356e5e6
Maximiliano Correa <maximiliano.correa@galenoargentina.com.ar>
2021-06-16 20:35:11 -0300

    From Template Method Refactoring (Motivation)

---
+++ src/main/java/dealingwithgeneralization/formtemplatemethod/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ * Tiene dos métodos en subclases que realizan pasos similares en el mismo orden, pero los pasos son
+ * diferentes.
+ * 
+ * Obtenga los pasos en métodos con la misma firma, de modo que los métodos originales sean los
+ * mismos. Entonces puedes subirlos a la superclass.
+ * 
+ */
+package dealingwithgeneralization.formtemplatemethod;
