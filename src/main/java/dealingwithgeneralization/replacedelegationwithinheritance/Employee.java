ed882993f31e726f58c181c5426fc3d046d90705
MaxiCorrea <jmaxicorrea@gmail.com>
2021-06-21 00:16:15 -0300

    Replace Delegation With Inheritance Example

--- src/main/java/dealingwithgeneralization/replacedelegationwithinheritance/Employee.java
+++ src/main/java/dealingwithgeneralization/replacedelegationwithinheritance/Employee.java
@@ -5,21 +5,6 @@ package dealingwithgeneralization.replacedelegationwithinheritance;
  * @author mxcorrea
  *
  */
-public class Employee {
-
-  Person person = new Person();
-
-  public String getName() {
-    return person.getName();
-  }
-
-  public void setName(
-      final String name) {
-    person.setName(name);
-  }
-
-  public Object getLastName() {
-    return person.getLastName();
-  }
-
+public class Employee extends Person {
+  
 }
