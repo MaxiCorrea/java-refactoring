b853de6fa05aa204abfb98899adc5759d9075d15
MaxiCorrea <jmaxicorrea@gmail.com>
2021-06-26 22:43:21 -0300

    Convert Procedural Design to Object.

---
+++ src/main/java/bigrefactorings/convertproceduraldesigntoobjects/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Tiene código escrito en un estilo procedimental.
+ * 
+ * Convierta los registros de datos en objetos, divida el comportamiento y transfiera el
+ * comportamiento a los objetos.
+ */
+package bigrefactorings.convertproceduraldesigntoobjects;
