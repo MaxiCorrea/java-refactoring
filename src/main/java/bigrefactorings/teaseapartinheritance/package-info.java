c74e0c7784a3e82b40bafa5cd62a2580c23f1524
MaxiCorrea <jmaxicorrea@gmail.com>
2021-06-26 22:29:06 -0300

    Adding package-info

---
+++ src/main/java/bigrefactorings/teaseapartinheritance/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Se burlan de la herencia
+ * 
+ * Tiene una jerarquía de herencia que realiza dos trabajos a la vez. Cree dos jerarquías y use la
+ * delegación para invocar una desde la otra.
+ */
+package bigrefactorings.teaseapartinheritance;
